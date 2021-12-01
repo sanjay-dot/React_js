e5c4048de2f9915631a12180ffb3cb27f97a5dda
Sanjay_Balaji <70127280+sanjay-dot@users.noreply.github.com>
2021-12-01 22:11:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import Login from './component/Login';
+
+function App() {
+    return ( 
+      <div>
+        <Header/>
+        
+        <Login/>
+        
+        </div>
+    )
+}
+
+export default App;
---
+++ mechanic.js
@@ -0,0 +1,10 @@
+const Mechanic =()=>{
+
+    return(
+        <div>
+            <h1>Mechanic sign up page</h1>
+        </div>
+
+    );
+}
+export default Mechanic;
