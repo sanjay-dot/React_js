7b0c63f7e4251fd3951d1f6bc60736882d2db5f0
Sanjay_Balaji <70127280+sanjay-dot@users.noreply.github.com>
2021-12-02 09:44:49 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+
+const Header =()=>{
+    return(
+        <div className="row">
+        <div className="container-fluid bg-dark p-3">
+            <div className="col text-light">
+                <h2 className ="mt-2 col">Find Mechanic</h2>        
+            </div>
+                    <div className ="col-md-3 float-right">
+                    <Link to="/user"><button className = "btn btn-primary">SignUp as User</button></Link>
+                    <Link to="/mechanic">  <button className = "btn btn-info ml-4">Signup as Mechanic</button></Link>
+                    </div> 
+             </div>
+        </div>
+        
+    );
+}
+
+export default Header;
